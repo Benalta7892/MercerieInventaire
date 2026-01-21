437faa17236ecf722c8577899e528693e66bd840
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2026-01-21 22:57:35 +0100

    feat: add associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :categories, dependent: :destroy
+  has_many :fournitures, dependent: :destroy
+  has_one :liste_achat, dependent: :destroy
 end
