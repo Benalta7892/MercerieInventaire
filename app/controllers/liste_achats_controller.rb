cf5d1537c24fb867789a3f8a843b31e1e0dd09fa
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2026-01-22 14:17:28 +0100

    feat: add liste_achat associations and user hook

--- app/controllers/liste_achats_controller.rb
+++ app/controllers/liste_achats_controller.rb
@@ -1,7 +1,7 @@
 class ListeAchatsController < ApplicationController
   before_action :authenticate_user!
   def show
-    @liste_achat = current_user.liste_achats
-    @items = @liste_achat.items.includes(:fourniture).order(created_at: :desc)
+    @liste_achat = current_user.liste_achat
+    @items = @liste_achat_items.includes(:fourniture).order(created_at: :desc)
   end
 end
--- app/models/liste_achat.rb
+++ app/models/liste_achat.rb
@@ -1,3 +1,5 @@
 class ListeAchat < ApplicationRecord
   belongs_to :user
+  has_many :liste_achat_items, dependent: :destroy
+  has_many :fournitures, through: :liste_achat_items
 end
